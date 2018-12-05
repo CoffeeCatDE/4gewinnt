e4d600d459621b981c0d33721b266f0cb63eeabc
Christian Schade <berlini@posteo.de>
2018-12-05 16:10:48 +0100

    Update app.js
    cleanup

--- app.js
+++ app.js
@@ -36,145 +36,6 @@
     var game = new Phaser.Game(config);
         
 
-
-
-     function Node(data) {
-        this.data = data;
-        this.parent = null;
-        this.children = [];
-    }
-     
-    function Tree(data) {
-        var node = new Node(data);
-        this._root = node;
-    }
-     
-    Tree.prototype.traverseDF = function(callback) {
-     
-        // this is a recurse and immediately-invoking function
-        (function recurse(currentNode) {
-            // step 2
-            for (var i = 0, length = currentNode.children.length; i < length; i++) {
-                // step 3
-                recurse(currentNode.children[i]);
-            }
-     
-            // step 4
-            callback(currentNode);
-     
-            // step 1
-        })(this._root);
-     
-    };
-     
-    Tree.prototype.traverseBF = function(callback) {
-        var queue = new Queue();
-     
-        queue.enqueue(this._root);
-     
-        currentTree = queue.dequeue();
-     
-        while(currentTree){
-            for (var i = 0, length = currentTree.children.length; i < length; i++) {
-                queue.enqueue(currentTree.children[i]);
-            }
-     
-            callback(currentTree);
-            currentTree = queue.dequeue();
-        }
-    };
-     
-    Tree.prototype.contains = function(callback, traversal) {
-        traversal.call(this, callback);
-    };
-     
-    Tree.prototype.add = function(data, toData, traversal) {
-        var child = new Node(data),
-            parent = null,
-            callback = function(node) {
-                if (node.data === toData) {
-                    parent = node;
-                }
-            };
-     
-        this.contains(callback, traversal);
-     
-        if (parent) {
-            parent.children.push(child);
-            child.parent = parent;
-        } else {
-            throw new Error('Cannot add node to a non-existent parent.');
-        }
-    };
- 
-    Tree.prototype.remove = function(data, fromData, traversal) {
-        var tree = this,
-            parent = null,
-            childToRemove = null,
-            index;
-     
-        var callback = function(node) {
-            if (node.data === fromData) {
-                parent = node;
-            }
-        };
-     
-        this.contains(callback, traversal);
-     
-        if (parent) {
-            index = findIndex(parent.children, data);
-     
-            if (index === undefined) {
-                throw new Error('Node to remove does not exist.');
-            } else {
-                childToRemove = parent.children.splice(index, 1);
-            }
-        } else {
-            throw new Error('Parent does not exist.');
-        }
-     
-        return childToRemove;
-    };
- 
-    function findIndex(arr, data) {
-        var index;
-     
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i].data === data) {
-                index = i;
-            }
-        }
-     
-        return index;
-    }
-
-
-
-    var tree = new Tree('CEO');
-     
-    tree.add('VP of Happiness', 'CEO', tree.traverseBF);
-    tree.add('VP of Finance', 'CEO', tree.traverseBF);
-    tree.add('VP of Sadness', 'CEO', tree.traverseBF);
-     
-    tree.add('Director of Puppies', 'VP of Finance', tree.traverseBF);
-    tree.add('Manager of Puppies', 'Director of Puppies', tree.traverseBF);
-
-
-     
-    /*
-     
-     tree
-     
-     'CEO'
-     ├── 'VP of Happiness'
-     ├── 'VP of Finance'
-     │   ├── 'Director of Puppies'
-     │   └── 'Manager of Puppies'
-     └── 'VP of Sadness'
-     
-     */
-
-
     function  preload ()
     {        
         this.load.image('background', 'assets/background.png');
@@ -229,30 +90,25 @@
 
     function hasOneWonVertical (whoIsPlayer){
 
-
-            for (var x = 0; x < width-1; x++){
-                for (var y = 5; y >= 3; y--){
-                    if (
-                        (gameField[y][x] == whoIsPlayer) &&
-                        (gameField[y-1][x] == whoIsPlayer) &&
-                        (gameField[y-2][x] == whoIsPlayer) &&
-                        (gameField[y-3][x] == whoIsPlayer)
-                        )                    
-                        {
+        for (var x = 0; x < width-1; x++){
+            for (var y = 5; y >= 3; y--){
+                if (
+                   (gameField[y][x] == whoIsPlayer) &&
+                   (gameField[y-1][x] == whoIsPlayer) &&
+                   (gameField[y-2][x] == whoIsPlayer) &&
+                   (gameField[y-3][x] == whoIsPlayer)
+                   )                    
+                {
                           
-                            if (whoIsPlayer == 1){
-                                        alert("You won!  (vertical)");
-                                        gameOver = true;
-                                    }
+                    if (whoIsPlayer == 1){
+                        alert("You won!  (vertical)");
+                   }
 
-                                    if (whoIsPlayer == 2){
-                                        gameOver = true;
-                                        alert("Computer won! (vertical)");
-                                    }
-
-                            gameOver = true;
-
-                        }
+                   if (whoIsPlayer == 2){
+                        alert("Computer won! (vertical)");                                
+                   }
+                        gameOver = true;
+                }
 
                     
             }
@@ -273,52 +129,18 @@
 
                                 if (whoIsPlayer == 1){
                                     alert("You won!  (horizontal)");
-                                    gameOver = true;
                                 }
 
                                 if (whoIsPlayer == 2){
-                                    gameOver = true;
                                     alert("You lost. (horizontal)");
                                 }
-                        }
+
+                                
+                                gameOver = true;
+                             }
                     
-        }
-    }
-
-
-
-
-
-
-/**
-
-        for (var x = 0; x < width-1; x++){
-            for (var y = 7; y > (heigth-4); y++){
-                 if (
-                    (gameField[y][x] == playerid) &&
-                    (gameField[y+1][x] == playerid) &&
-                    (gameField[y+2][x] == playerid) &&
-                    (gameField[y+3][x] == playerid)
-                    ){
-
-                        if (playerid == 1){
-                                alert("Game Over! You won! (vertical)");
-                                gameOver = true;
-                                break;
-                       }
-
-                        if (playerid == 2){
-                                gameOver = true;
-                                alert("Game Over! You lost. (Vertical)");
-                                break;
-                        }
-                    }
-                
             }
-
         }
-
-        **/
     }
 
 
